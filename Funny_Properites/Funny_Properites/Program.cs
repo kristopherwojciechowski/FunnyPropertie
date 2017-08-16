b9f88aba535165493777106a25d38c2c5963c456
Kristopher Wojciechowski <kristopher.wojciechowski@gmail.com>
2017-08-16 22:16:04 +0200

    developing

--- Funny_Properites/Funny_Properites/Program.cs
+++ Funny_Properites/Funny_Properites/Program.cs
@@ -10,12 +10,23 @@ namespace Funny_Properites
     {
         static void Main(string[] args)
         {
-            DigPow.digPow(2, 2);
-            Console.WriteLine("podajliczbe:");
+            //DigPow.digPow(2, 2);
+            Console.WriteLine("podaj liczbe:");
             String positivIntegerN = Console.ReadLine();
-            String k = positivIntegerN.Substring(0, 1);
-            Console.WriteLine("Substring: {0}", k);
-            Console.WriteLine("dlugosc: {0}", positivIntegerN.Length);
+            //String k = positivIntegerN.Substring(0, 1);
+            //Console.WriteLine("Substring: {0}", k);
+            int p = 1;
+            int tempNum, k;
+            double sum = 0;
+            for (int m = 1; m < positivIntegerN.Length; m++)
+            {
+                tempNum = Int32.Parse(positivIntegerN.Substring(m-1, m));
+                Console.WriteLine("selected number: {0}", tempNum);
+                sum = sum + Math.Pow(Convert.ToDouble(tempNum),Convert.ToDouble(p+m-1));
+                Console.WriteLine("suma po {0}-tej iteracji: {1}", m, sum);
+            }
+            Console.WriteLine("ilosc znakow w liczbie: {0}", positivIntegerN.Length);
+            Console.WriteLine("suma: {0}", sum);
         }
     }
 }

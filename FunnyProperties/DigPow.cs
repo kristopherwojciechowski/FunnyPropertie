33b935ffab85bbac570f28a1b359b85fd3c0affa
Kristopher Wojciechowski <kristopher.wojciechowski@gmail.com>
2017-08-16 12:37:47 +0200

    star files

---
+++ FunnyProperties/DigPow.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunnyProperties
+{
+    public class DigPow
+    {
+        public static long digPow(int n, int p)
+        {
+
+            return -1;
+        }
+    }
+}

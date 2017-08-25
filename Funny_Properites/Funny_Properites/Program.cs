fa0926e4003c18a31acb0ba74a35cd748f5fef1c
Kristopher Wojciechowski <kristopher.wojciechowski@gmail.com>
2017-08-25 13:22:49 +0200

    better solution

--- Funny_Properites/Funny_Properites/Program.cs
+++ Funny_Properites/Funny_Properites/Program.cs
@@ -121,55 +121,7 @@ namespace Funny_Properites
             Fighter winner = new Fighter();
 
             Console.WriteLine();
-            #region  this part is replaced with methode from Fighter class
-            //int hit1, hit2, i = 0;
-            //do
-            //{
-            //    hit1 = rand.Next(10);
-            //    hit2 = rand.Next(10);
-            //    if (hit1 > hit2)
-            //    {
-            //        Console.WriteLine("hit_1 = {0}, hit_2 = {1}", hit1, hit2);
-            //        fighter02.health = fighter02.health - hit1;
-            //        Console.WriteLine("john health: " + fighter02.health);
-            //    }
-            //    else
-            //    {
-            //        Console.WriteLine("hit 2 > hit 1");
-            //    }
-
-            //    hit1 = rand.Next(10);
-            //    hit2 = rand.Next(10);
-            //    if (hit1 < hit2)
-            //    {
-            //        Console.WriteLine("hit_1 = {0}, hit_2 = {1}", hit1, hit2);
-            //        fighter01.health = fighter01.health - hit2;
-            //        Console.WriteLine("jas health: " + fighter01.health);
-            //    }
-            //    else
-            //    {
-            //        Console.WriteLine("hit 1 > hit 2");
-            //    }
-            //    i++;
-            //    Console.WriteLine("koniec rundy {0}", i);
-            //    Console.WriteLine();
-
-            //    if (fighter01.health <= 0)
-            //    {
-            //        winner.name = fighter02.name;
-            //    }
-
-            //    if (fighter02.health <= 0)
-            //    {
-            //        winner.name = fighter01.name;
-            //    }
-
-            //} while (fighter01.health > 0 && fighter02.health > 0);
-
-            //Console.WriteLine();
-            //Console.WriteLine("The winner is: " + winner.name);
-
-            #endregion
+            
             string decision;
             do
             {
@@ -192,7 +144,8 @@ namespace Funny_Properites
                     winner = Fighter.fight(fighter01, fighter02);
                     Console.WriteLine("The winner is: " + winner.name);
                     Console.WriteLine();
-                    if (winner == fighter01)
+
+                    if (winner.name == fighter01.name)
                     {
                         fighter01.health = fighter01Health + 5;
                         Console.WriteLine("{0} health = {1}", fighter01.name, fighter01.health);
